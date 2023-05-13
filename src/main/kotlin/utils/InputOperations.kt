83fd68928237a81d6772bbe277d0f4fa7253f906
rolandsarosy <rolandsarosy@gmail.com>
2023-05-13 17:43:15 +0200

    Refactor division by zero check

--- src/main/kotlin/utils/InputOperations.kt
+++ src/main/kotlin/utils/InputOperations.kt
@@ -13,7 +13,7 @@ object InputOperations {
     private const val VALID_OPERATION_ELEMENTS_SIZE = 3
     private const val VALID_OPERAND_REGEX = """^-?(\d+&\d+/[1-9]\d*|\d+/[1-9]\d*|\d+)$"""
 
-    @JvmStatic // Assumes any input, returns valid operation format.
+    @JvmStatic // Assumes any input.
     fun parseInput(input: String) = when {
         input.isEmpty() -> ERROR_EMPTY_MESSAGE
         !input.containsElementFromList(LEGAL_OPERATORS.keys.toList()) -> ERROR_NO_OPERATOR
@@ -22,7 +22,7 @@ object InputOperations {
         else -> parseOperation(input)
     }
 
-    @JvmStatic // Assumes a valid operation format, returns operands and operator.
+    @JvmStatic // Assumes a valid operation format.
     fun parseOperation(operationStr: String): String {
         val elements = splitOperation(operationStr)
         val operand1 = if (isOperandValid(elements[0])) elements[0] else return ERROR_INVALID_FIRST_OPERAND
@@ -31,7 +31,7 @@ object InputOperations {
         return performOperation(elements[1][0], Fraction.fromString(operand1), Fraction.fromString(operand2))
     }
 
-    @JvmStatic // Assumes a valid operation, returns operation result.
+    @JvmStatic // Assumes a valid operation.
     private fun performOperation(operator: Char, operand1: Fraction, operand2: Fraction): String {
         val operation = LEGAL_OPERATORS[operator] ?: throw IllegalArgumentException("Invalid operator: $operator")
         return operation(operand1, operand2).toString()
@@ -49,13 +49,14 @@ object InputOperations {
     fun isDivisionByZero(operationStr: String): Boolean {
         val elements = splitOperation(operationStr.replace("-", ""))
         if (elements[1] != "/") return false
-        if (elements[2].toIntOrNull() == 0) return true
 
+        val isWholeZero = elements[2].toIntOrNull() == 0
         val fractionParts = elements[2].split("/")
-        if (fractionParts.size == 2 && fractionParts[0] == "0") return true
-
+        val isProperZero = fractionParts.size == 2 && fractionParts[0] == "0"
         val improperFractionParts = elements[2].split("&")
-        return improperFractionParts.size == 2 && improperFractionParts[0] == "0" && improperFractionParts[1].split("/")[0] == "0"
+        val isImproperZero = improperFractionParts.size == 2 && improperFractionParts[0] == "0" && improperFractionParts[1].split("/")[0] == "0"
+
+        return isWholeZero || isProperZero || isImproperZero
     }
 
     @JvmStatic
